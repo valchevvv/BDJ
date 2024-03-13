c63f192756b50ba2b5ce4ee5c05470a19d53ad0e
Erol <osmanoverol@gmail.com>
2024-03-13 12:04:16 +0200

    Info

---
+++ Заявка за 6.sql
@@ -0,0 +1,58 @@
+create database bdz12v_1	
+
+use bdz12v_1
+
+create table Cities(
+id int primary key IDENTITY,
+name nvarchar(40)
+)
+
+create table Trains(
+id int primary key IDENTITY,
+places int,
+type nvarchar(20)
+)
+
+create table Routes(
+id int primary key IDENTITY,
+train int foreign key references Trains(id),
+start_date datetime,
+end_date datetime,
+normal_price decimal,
+first_class decimal default null,
+sleeper_class decimal default null
+)
+
+create table Route_Stops(
+route int foreign key references Routes(id),
+city int foreign key references Cities(id),
+number int,
+arrival datetime
+)
+
+create table Users(
+id int primary key IDENTITY,
+username nvarchar(40),
+password text,
+isAdmin bit default 0
+)
+
+create table Reservations(
+id int primary key IDENTITY,
+passenger int foreign key references Users(id),
+route int foreign key references Routes(id),
+board int foreign key references Cities(id),
+arrive int foreign key references Cities(id)
+)
+
+
+
+
+
+
+
+
+
+
+
+
